fc16b985b8f924dceb5d588758b7c92806ec0527
Soumyadeepaiml2B64 <Soumyadeep.dolui2024@uem.edu.in>
2025-07-17 06:38:10 +0000

    jj

---
+++ spiral.py
@@ -0,0 +1,30 @@
+rows = int(input("Enter the number of rows: "))
+cols = int(input("Enter the number of columns: "))
+matrix = []
+top = 0; bottom = rows - 1
+left = 0; right = cols - 1
+
+print("Input matrix:")
+for i in range(rows):
+    for j in range(cols):
+        value = int(input(f"Element [{i}][{j}]: "))
+
+while (top <= bottom and left <= right):
+
+    for i in range(left, right):
+        print(matrix[top][i], end=' ')
+    top += 1
+
+    for i in range(top, bottom):
+        print(matrix[i][right], end=' ')
+    right -= 1
+
+    if top <= bottom:
+        for i in range(right, left , -1):
+            print(matrix[bottom][i], end=' ')
+        bottom -= 1
+
+    if left <= right:
+        for i in range(bottom, top , -1):
+            print(matrix[i][left], end=' ')
+        left += 1
